b130cf5544e0845387d527e3da6f1729c8fc77c8
khubaib Ayubi Khubaib <akbarayubikhubaib@gmail.com>
2025-01-02 19:13:06 +0500

    Initial push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {ButtonModule} from 'primeng/button';
+import { ProductsComponent } from './products/products.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ButtonModule,ProductsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
